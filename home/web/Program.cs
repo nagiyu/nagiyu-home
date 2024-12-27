4278114a2e27a3fac3cb0e885c71b7f9e5ed6e62
nagiyu <otonagiyu@gmail.com>
2024-12-28 00:46:43 +0900

    Add Policy module with Privacy and Terms controllers, views, and project references

--- home/web/Program.cs
+++ home/web/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Nagiyu.Auth.Web.Controllers;
 using Nagiyu.Auth.Web.Middlewares;
 using Nagiyu.Common.Auth.Service.Services;
+using Nagiyu.Policy.Web.Controllers;
 using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -39,7 +40,8 @@ builder.WebHost.ConfigureKestrel(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
-    .AddApplicationPart(typeof(AccountController).Assembly);
+    .AddApplicationPart(typeof(AccountController).Assembly)
+    .AddApplicationPart(typeof(PolicyController).Assembly);
 
 builder.Services
     .AddAuthentication(options =>
---
+++ policy/web/Controllers/PolicyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nagiyu.Policy.Web.Controllers
+{
+    public class PolicyController : Controller
+    {
+        /// <summary>
+        /// プライバシーポリシー
+        /// </summary>
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 利用規約
+        /// </summary>
+        public IActionResult Terms()
+        {
+            return View();
+        }
+    }
+}
