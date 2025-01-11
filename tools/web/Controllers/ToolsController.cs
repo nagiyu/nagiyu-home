b52f2bd5ad9a09ff99a7ef88573b0e453b80e419
nagiyu <otonagiyu@gmail.com>
2025-01-11 15:56:34 +0900

    Add Tools project and integrate with main application

---
+++ tools/web/Controllers/ToolsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nagiyu.Tools.Web.Controllers
+{
+    public class ToolsController : Controller
+    {
+        /// <summary>
+        /// 乗り換え変換
+        /// </summary>
+        [Route("tools/convert-yahoo-transit")]
+        public IActionResult ConvertYahooTransit()
+        {
+            return View();
+        }
+    }
+}
