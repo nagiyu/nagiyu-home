2518dca8cfeb5f9c7d64aa59780617874cee7dd3
nagiyu <otonagiyu@gmail.com>
2025-01-25 17:27:48 +0900

    Make ChangeNotifyCarouselStatus asynchronous and await its execution in StartupModal; update BindSubscriptionId to await optIn and log subscription ID

--- common/client/components/modals/StartupModal.vue
+++ common/client/components/modals/StartupModal.vue
@@ -203,7 +203,7 @@ class StartupModal extends Vue {
     this.ChangePWACaroueselStatus();
     this.ChangeConfirmCaroueselStatus();
     await this.ChangeLoginCaroueselStatus();
-    this.ChangeNotifyCaroueselStatus();
+    await this.ChangeNotifyCaroueselStatus();
 
     await this.ChangePrevButtonStatus();
     await this.ChangeNextButtonStatus();
@@ -242,7 +242,7 @@ class StartupModal extends Vue {
     this.ChangePWACaroueselStatus();
     this.ChangeConfirmCaroueselStatus();
     await this.ChangeLoginCaroueselStatus();
-    this.ChangeNotifyCaroueselStatus();
+    await this.ChangeNotifyCaroueselStatus();
 
     var count = 0;
 
@@ -296,9 +296,10 @@ class StartupModal extends Vue {
   /**
    * 通知を勧めるカルーセルの状態を変更する
    */
-  private ChangeNotifyCaroueselStatus(): void {
-    console.log(`DEBUG::: optedIn: ${this.$OneSignal.User.PushSubscription.optedIn}`);
+  private async ChangeNotifyCaroueselStatus(): Promise<void> {
+    await this.$OneSignal.User.PushSubscription.optIn();
     var subscriptionId = this.$OneSignal.User.PushSubscription.id;
+    console.log(`DEBUG::: id in Common.vue: ${this.$OneSignal.User.PushSubscription.id}`);
 
     if (subscriptionId !== null && subscriptionId !== undefined) {
       this.isEnabledNotifyCarouesel = false;
--- common/client/views/Common.vue
+++ common/client/views/Common.vue
@@ -106,8 +106,9 @@ class Common extends Vue {
    * OneSignal の SubscriptionId をバインドする
    */
   private async BindSubscriptionId(): Promise<void> {
-    console.log(`DEBUG::: optedIn in Common.vue: ${this.$OneSignal.User.PushSubscription.optedIn}`);
+    await this.$OneSignal.User.PushSubscription.optIn();
     var subscriptionId = this.$OneSignal.User.PushSubscription.id;
+    console.log(`DEBUG::: id in Common.vue: ${this.$OneSignal.User.PushSubscription.id}`);
 
     if (!subscriptionId) {
       return;
