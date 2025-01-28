6bf5cee71e1ea57062e15986aaef59fbc3bf5b7b
nagiyu <otonagiyu@gmail.com>
2025-01-28 15:42:23 +0900

    Improve debug logging in LoginItem component for better clarity

--- common/client/components/stepItems/LoginItem.vue
+++ common/client/components/stepItems/LoginItem.vue
@@ -158,7 +158,8 @@ class LoginItem extends StepItemBase {
     this.isLogin = this.user !== null;
     this.isSubscribeConnected = this.user !== null && this.user.oneSignalSubscriptionId === this.subscriptionId;
 
-    console.log('DEBUG::: User: ', this.user);
+    console.log('DEBUG::: User: ');
+    console.log(this.user);
     console.log(`DEBUG::: SubscriptionId: ${this.subscriptionId}`);
   }
 }
