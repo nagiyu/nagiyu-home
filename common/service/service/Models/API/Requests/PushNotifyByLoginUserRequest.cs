b96c463323275f82f1e370211bec2d57fd9406f4
nagiyu <otonagiyu@gmail.com>
2025-01-28 11:56:59 +0900

    Refactor Notification structure: rename and move NotificationAPIController, update project references, and add request models for push notifications

---
+++ common/service/service/Models/API/Requests/PushNotifyByLoginUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Nagiyu.Common.Service.Models.API.Requests
+{
+    public class PushNotifyByLoginUserRequest
+    {
+        /// <summary>
+        /// メッセージ
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ common/service/service/Models/API/Requests/PushNotifyBySubscriptionIdRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Nagiyu.Common.Service.Models.API.Requests
+{
+    public class PushBySubscriptionIdsRequest
+    {
+        /// <summary>
+        /// SubscriptionIds
+        /// </summary>
+        public List<string> SubscriptionIds { get; set; }
+
+        /// <summary>
+        /// メッセージ
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
