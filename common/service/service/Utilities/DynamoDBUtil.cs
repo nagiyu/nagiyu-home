e6469f002f61e5b69b13e6dc09690538a660d633
nagiyu <otonagiyu@gmail.com>
2025-03-12 16:24:09 +0900

    Enhance DynamoDB utility methods to include optional parameter for handling missing keys

--- common/service/service/Utilities/DynamoDBUtil.cs
+++ common/service/service/Utilities/DynamoDBUtil.cs
@@ -6,52 +6,52 @@ namespace Nagiyu.Common.Service.Utilities
 {
     public static class DynamoDBUtil
     {
-        public static string GetStringValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static string GetStringValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => v.S);
+            return GetValueOrThrow(keyValuePairs, key, v => v.S, throwIfNotFound);
         }
 
-        public static List<string> GetStringListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static List<string> GetStringListValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => v.SS);
+            return GetValueOrThrow(keyValuePairs, key, v => v.SS, throwIfNotFound);
         }
 
-        public static int GetIntValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static int GetIntValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => int.Parse(v.N));
+            return GetValueOrThrow(keyValuePairs, key, v => int.Parse(v.N), throwIfNotFound);
         }
 
-        public static List<int> GetIntListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static List<int> GetIntListValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(int.Parse));
+            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(int.Parse), throwIfNotFound);
         }
 
-        public static long GetLongValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static long GetLongValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => long.Parse(v.N));
+            return GetValueOrThrow(keyValuePairs, key, v => long.Parse(v.N), throwIfNotFound);
         }
 
-        public static List<long> GetLongListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static List<long> GetLongListValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(long.Parse));
+            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(long.Parse), throwIfNotFound);
         }
 
-        public static double GetDoubleValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static double GetDoubleValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => double.Parse(v.N));
+            return GetValueOrThrow(keyValuePairs, key, v => double.Parse(v.N), throwIfNotFound);
         }
 
-        public static List<double> GetDoubleListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static List<double> GetDoubleListValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(double.Parse));
+            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(double.Parse), throwIfNotFound);
         }
 
-        public static bool GetBoolValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        public static bool GetBoolValue(Dictionary<string, AttributeValue> keyValuePairs, string key, bool throwIfNotFound = true)
         {
-            return GetValueOrThrow(keyValuePairs, key, v => v.BOOL);
+            return GetValueOrThrow(keyValuePairs, key, v => v.BOOL, throwIfNotFound);
         }
 
-        private static T GetValueOrThrow<T>(Dictionary<string, AttributeValue> keyValuePairs, string key, Func<AttributeValue, T> selector)
+        private static T GetValueOrThrow<T>(Dictionary<string, AttributeValue> keyValuePairs, string key, Func<AttributeValue, T> selector, bool throwIfNotFound)
         {
             if (keyValuePairs.TryGetValue(key, out var value))
             {
@@ -66,7 +66,14 @@ namespace Nagiyu.Common.Service.Utilities
             }
             else
             {
-                throw new KeyNotFoundException(key);
+                if (throwIfNotFound)
+                {
+                    throw new KeyNotFoundException(key);
+                }
+                else
+                {
+                    return default;
+                }
             }
         }
     }
