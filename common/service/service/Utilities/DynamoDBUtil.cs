2f0e7191110f7d0ba8f3dc9854830cfe85cb1125
nagiyu <otonagiyu@gmail.com>
2025-03-12 16:00:48 +0900

    Add DynamoDB utility methods for attribute value retrieval and refactor TestRecord initialization

---
+++ common/service/service/Utilities/DynamoDBUtil.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.DynamoDBv2.Model;
+
+namespace Nagiyu.Common.Service.Utilities
+{
+    public static class DynamoDBUtil
+    {
+        public static string GetStringValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => v.S);
+        }
+
+        public static List<string> GetStringListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => v.SS);
+        }
+
+        public static int GetIntValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => int.Parse(v.N));
+        }
+
+        public static List<int> GetIntListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(int.Parse));
+        }
+
+        public static long GetLongValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => long.Parse(v.N));
+        }
+
+        public static List<long> GetLongListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(long.Parse));
+        }
+
+        public static double GetDoubleValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => double.Parse(v.N));
+        }
+
+        public static List<double> GetDoubleListValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => v.NS.ConvertAll(double.Parse));
+        }
+
+        public static bool GetBoolValue(Dictionary<string, AttributeValue> keyValuePairs, string key)
+        {
+            return GetValueOrThrow(keyValuePairs, key, v => v.BOOL);
+        }
+
+        private static T GetValueOrThrow<T>(Dictionary<string, AttributeValue> keyValuePairs, string key, Func<AttributeValue, T> selector)
+        {
+            if (keyValuePairs.TryGetValue(key, out var value))
+            {
+                try
+                {
+                    return selector(value);
+                }
+                catch (Exception)
+                {
+                    throw new InvalidCastException(key);
+                }
+            }
+            else
+            {
+                throw new KeyNotFoundException(key);
+            }
+        }
+    }
+}
--- common/service/tests/Services/DynamoDBServiceTest.cs
+++ common/service/tests/Services/DynamoDBServiceTest.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.Model;
 using Microsoft.Extensions.Configuration;
 using Nagiyu.Common.Service.Models.DB;
 using Nagiyu.Common.Service.Services;
+using Nagiyu.Common.Service.Utilities;
 
 namespace Nagiyu.Common.Service.Tests.Services
 {
@@ -56,146 +56,21 @@ namespace Nagiyu.Common.Service.Tests.Services
         [DynamoDBProperty]
         public bool BoolColumn { get; set; }
 
-        [DynamoDBProperty]
-        public string IndexId { get; set; }
-
-        [DynamoDBProperty]
-        public string IndexColumn1 { get; set; }
-
         public TestRecord() : base()
         {
         }
 
         public TestRecord(Dictionary<string, AttributeValue> keyValuePairs) : base(keyValuePairs)
         {
-            if (keyValuePairs.TryGetValue(nameof(Column1), out var column1) && column1.S != null)
-            {
-                Column1 = column1.S;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(Column1));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(StringColumns), out var stringColumns) && stringColumns.IsSSSet)
-            {
-                StringColumns = stringColumns.SS;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(StringColumns));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(IntColumn), out var intColumn) && int.TryParse(intColumn.N, out var intColumnValue))
-            {
-                IntColumn = intColumnValue;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(IntColumn));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(IntColumns), out var intColumns) && intColumns.IsNSSet)
-            {
-                foreach (var value in intColumns.NS)
-                {
-                    if (int.TryParse(value, out var intValue))
-                    {
-                        IntColumns.Add(intValue);
-                    }
-                    else
-                    {
-                        throw new InvalidCastException(nameof(IntColumns));
-                    }
-                }
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(IntColumns));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(LongColumn), out var longColumn) && long.TryParse(longColumn.N, out var longColumnValue))
-            {
-                LongColumn = longColumnValue;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(LongColumn));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(LongColumns), out var longColumns) && intColumns.IsNSSet)
-            {
-                foreach (var value in longColumns.NS)
-                {
-                    if (long.TryParse(value, out var intValue))
-                    {
-                        LongColumns.Add(intValue);
-                    }
-                    else
-                    {
-                        throw new InvalidCastException(nameof(LongColumns));
-                    }
-                }
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(LongColumns));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(DoubleColumn), out var doubleColumn) && double.TryParse(doubleColumn.N, out var doubleColumnValue))
-            {
-                DoubleColumn = doubleColumnValue;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(DoubleColumn));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(DoubleColumns), out var doubleColumns) && doubleColumns.IsNSSet)
-            {
-                foreach (var value in doubleColumns.NS)
-                {
-                    if (double.TryParse(value, out var doubleValue))
-                    {
-                        DoubleColumns.Add(doubleValue);
-                    }
-                    else
-                    {
-                        throw new InvalidCastException(nameof(DoubleColumns));
-                    }
-                }
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(DoubleColumns));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(BoolColumn), out var boolColumn) && boolColumn.IsBOOLSet)
-            {
-                BoolColumn = boolColumn.BOOL;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(BoolColumn));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(IndexId), out var indexId) && indexId.S != null)
-            {
-                IndexId = indexId.S;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(IndexId));
-            }
-
-            if (keyValuePairs.TryGetValue(nameof(IndexColumn1), out var indexColumn1) && indexColumn1.S != null)
-            {
-                IndexColumn1 = indexColumn1.S;
-            }
-            else
-            {
-                throw new KeyNotFoundException(nameof(IndexColumn1));
-            }
+            Column1 = DynamoDBUtil.GetStringValue(keyValuePairs, nameof(Column1));
+            StringColumns = DynamoDBUtil.GetStringListValue(keyValuePairs, nameof(StringColumns));
+            IntColumn = DynamoDBUtil.GetIntValue(keyValuePairs, nameof(IntColumn));
+            IntColumns = DynamoDBUtil.GetIntListValue(keyValuePairs, nameof(IntColumns));
+            LongColumn = DynamoDBUtil.GetLongValue(keyValuePairs, nameof(LongColumn));
+            LongColumns = DynamoDBUtil.GetLongListValue(keyValuePairs, nameof(LongColumns));
+            DoubleColumn = DynamoDBUtil.GetDoubleValue(keyValuePairs, nameof(DoubleColumn));
+            DoubleColumns = DynamoDBUtil.GetDoubleListValue(keyValuePairs, nameof(DoubleColumns));
+            BoolColumn = DynamoDBUtil.GetBoolValue(keyValuePairs, nameof(BoolColumn));
         }
     }
 
@@ -210,14 +85,7 @@ namespace Nagiyu.Common.Service.Tests.Services
 
         public ExtendedTestRecord(Dictionary<string, AttributeValue> keyValuePairs) : base(keyValuePairs)
         {
-            if (keyValuePairs.TryGetValue(nameof(Column2), out var column2) && column2.S != null)
-            {
-                Column2 = column2.S;
-            }
-            else
-            {
-                Column2 = null;
-            }
+            Column2 = DynamoDBUtil.GetStringValue(keyValuePairs, nameof(Column2));
         }
     }
 
